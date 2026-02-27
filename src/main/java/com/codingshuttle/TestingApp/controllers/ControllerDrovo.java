6419f5fd2164830004b92794868a67910d91e5a0
drovo <mahdian.drovo@gmail.com>
2026-02-27 21:58:43 +0100

    drovo is added

---
+++ src/main/java/com/codingshuttle/TestingApp/controllers/ControllerDrovo.java
@@ -0,0 +1,20 @@
+package com.codingshuttle.TestingApp.controllers;
+
+import com.codingshuttle.TestingApp.dto.EmployeeDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/drovo")
+public class ControllerDrovo {
+    @GetMapping("/drovo-info")
+    public ResponseEntity<EmployeeDto> getDrovoInfo() {
+
+        EmployeeDto drovo = new EmployeeDto();
+        drovo.setName("Mah Dian Drovo");
+        drovo.setEmail("mahdian@example.com");
+        drovo.setSalary(50000L);
+
+        return ResponseEntity.ok(drovo);
+    }
+}
